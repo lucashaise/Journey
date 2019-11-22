0082b33080a5672d14465f3fc2ca83c004e4a141
haise <56767062+lucashaise@users.noreply.github.com>
2019-11-22 19:39:08 -0300

    Add files via upload

---
+++ 2universe.js
@@ -0,0 +1,22 @@
+
+function myFunction() {
+  location.href = "#top";
+}
+
+  
+function gotowhat() {
+  location.href = "#whatis";
+}
+
+function gotot() {
+  location.href = "#thery";
+}
+
+  
+function gotoceli() {
+  location.href = "#celi";
+}
+
+ function go() {
+  location.href = "#o";
+}
